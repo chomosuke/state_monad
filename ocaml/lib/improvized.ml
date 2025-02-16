87abf32efdfab0c9f6203bc428f4f5e92400601f
chomosuke <a13323600@gmail.com>
2025-02-16 16:39:13 +0800

    identical

--- ocaml/lib/improvized.ml
+++ ocaml/lib/improvized.ml
@@ -1,36 +1,22 @@
 open Core
 
 module T = struct
-  type 'a t =
-    | Nested :
-        { f : 'b -> 'a t
-        ; prev : 'b t
-        }
-        -> 'a t
-    | Pure of 'a
-    | Get : int t
-    | Set : int -> unit t
+  type 'a t = int -> 'a * int
 
-  let bind m ~f = Nested { f; prev = m }
-  let return x = Pure x
+  let bind m ~f =
+    fun s ->
+    let x, s = m s in
+    let m = f x in
+    m s
+  ;;
+
+  let return x = fun s -> x, s
   let map = `Define_using_bind
 end
 
 include T
 include Monad.Make (T)
 
-let get = Get
-let set s = Set s
-
-let rec run : type a. a t -> int -> a * int =
-  fun m ->
-  match m with
-  | Nested { f; prev } ->
-    fun s ->
-      let x, s = run prev s in
-      let m = f x in
-      run m s
-  | Pure x -> fun s -> x, s
-  | Get -> fun s -> s, s
-  | Set x -> fun _s -> (), x
-;;
+let get = fun s -> s, s
+let set s = fun _ -> (), s
+let run : type a. a t -> int -> a * int = fun m -> m
